b259cac103071a39c3c8e29a694be2930dc1b972
sud-vastav <sudvastavs@gmail.com>
2015-07-03 11:21:08 +0530

    minimal-ify
    minimal-ify

--- minimal.mk
+++ minimal.mk
@@ -1,9 +1,9 @@
-# Inherit some common Candy5 stuff
-$(call inherit-product, vendor/candy5/config/common_full_phone.mk)
+# Inherit some common minimak stuff
+$(call inherit-product, vendor/minimal/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/armani/full_armani.mk)
 
-PRODUCT_NAME := candy5_armani
+PRODUCT_NAME := minimal_armani
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
